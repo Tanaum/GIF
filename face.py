f26068953df9b81bbc9bfc2716eab81b613ecd29
least_smart <tanaumbutt@gmail.com>
2023-06-28 17:49:47 +0500

    Update face.py
    Tried to make it of three pictures, couldn't do that :(

--- face.py
+++ face.py
@@ -7,6 +7,9 @@ for arg in sys.argv[1:]:
     img = Image.open(arg)
     images.append(img)
 
-images[0].save(
-    'face.gif', save_all = True, append_images=[images[2]], duration = 200, loop = 0
-)
+i = 0
+while i != 1:
+    images[i].save(
+        'face.gif', save_all = True, append_images=[images[1+1]], duration = 200, loop = 0
+    )
+    i+=1
